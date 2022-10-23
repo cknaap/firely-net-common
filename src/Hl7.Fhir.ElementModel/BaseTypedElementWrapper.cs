f090f7c3679f1a4e7b0a254eced75a8244fdb9f2
Christiaan Knaap <christiaanfurore@outlook.com>
2022-10-23 07:47:13 +0200

    Cache children of ITypedElement

---
+++ src/Hl7.Fhir.ElementModel/BaseTypedElementWrapper.cs
@@ -0,0 +1,61 @@
+﻿using Hl7.Fhir.ElementModel;
+using Hl7.Fhir.Specification;
+using Hl7.Fhir.Utility;
+using System;
+using System.Collections.Generic;
+
+namespace Hl7.Fhir.ElementModel
+{
+    public abstract class BaseTypedElementWrapper : ITypedElement, IAnnotated, IExceptionSource
+    {
+        protected readonly ITypedElement wrapped;
+
+        public BaseTypedElementWrapper(ITypedElement original)
+        {
+            wrapped = original;
+
+            if (original is IExceptionSource ies && ies.ExceptionHandler == null)
+                ies.ExceptionHandler = (o, a) => exHandler.NotifyOrThrow(o, a); // Only call the exception handler that might have been added to the wrapped element, don't reach out to the original element. Otherwise we'll create a recursion.
+        }
+
+        public virtual string Name => wrapped.Name;
+
+        public virtual string InstanceType => wrapped.InstanceType;
+
+        public virtual object Value => wrapped.Value;
+
+        public virtual string Location => wrapped.Location;
+
+        public virtual IElementDefinitionSummary Definition => wrapped.Definition;
+
+        public virtual IEnumerable<ITypedElement> Children(string name = null)
+        {
+            return wrapped.Children(name);
+        }
+
+        #region IExceptionSource implementation
+        protected ExceptionNotificationHandler exHandler = null;
+        public virtual ExceptionNotificationHandler ExceptionHandler
+        {
+            get => exHandler ?? (wrapped as IExceptionSource)?.ExceptionHandler;
+            set => exHandler = value;
+        }
+
+        protected virtual void NotifyOrThrow(ExceptionNotification notification)
+        {
+            ExceptionHandler.NotifyOrThrow(this, notification);
+        }
+        #endregion
+
+        #region IAnnoted implementation
+        public virtual IEnumerable<object> Annotations(Type type)
+        {
+            if (type == typeof(ITypedElement))
+            {
+                return new[] { this }; //Override previous implementations of ITypedElement, since an override of this class probably changed something to one of the properties.
+            }
+            return (wrapped as IAnnotated)?.Annotations(type) ?? new object[] { };
+        }
+        #endregion
+    }
+}
---
+++ src/Hl7.Fhir.ElementModel/CachedElement.cs
@@ -0,0 +1,97 @@
+﻿using Hl7.Fhir.ElementModel;
+using Hl7.Fhir.Specification;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hl7.Fhir.ElementModel
+{
+    internal class CachedElement : BaseTypedElementWrapper
+    {
+        public CachedElement(ITypedElement original) : base(original)
+        {
+        }
+
+        private bool _nameRead = false;
+        private string _name = null;
+        public override string Name
+        {
+            get
+            {
+                _name = _nameRead ? _name : base.Name;
+                _nameRead = true;
+                return _name;
+            }
+        }
+
+        private bool _valueRead = false;
+        private object _value = null;
+        public override object Value
+        {
+            get
+            {
+                _value = _valueRead ? _value : base.Value;
+                _valueRead = true;
+                return _value;
+            }
+        }
+
+        private bool _locationRead = false;
+        private string _location = null;
+        public override string Location
+        {
+            get
+            {
+                _location = _locationRead ? _location : base.Location;
+                _locationRead = true;
+                return _location;
+            }
+        }
+
+        private bool _instanceTypeRead = false;
+        private string _instanceType = null;
+        public override string InstanceType
+        {
+            get
+            {
+                _instanceType = _instanceTypeRead ? _instanceType : base.InstanceType;
+                _instanceTypeRead = true;
+                return _instanceType;
+            }
+        }
+
+        private bool _definitionRead = false;
+        private IElementDefinitionSummary _definition = null;
+        public override IElementDefinitionSummary Definition
+        {
+            get
+            {
+                _definition = _definitionRead ? _definition : base.Definition;
+                _definitionRead = true;
+                return _definition;
+            }
+        }
+
+        private bool _childrenRead = false;
+        private ILookup<string, ITypedElement> _children;
+
+        public override IEnumerable<ITypedElement> Children(string name = null)
+        {
+            _children = _childrenRead ? _children : wrapped.Children().ToLookup(child => child.Name, child => child.Cache());
+            _childrenRead = true;
+            return (name is null) ? _children.SelectMany(group => group) : _children[name];
+        }
+    }
+
+    public static partial class ITypedElementExtensions
+    {
+        /// <summary>
+        /// Prevent recalculation of the Children upon every access.
+        /// </summary>
+        /// <param name="original"></param>
+        /// <returns></returns>
+        public static ITypedElement Cache(this ITypedElement original)
+        {
+            return original as CachedElement ?? new CachedElement(original);
+        }
+    }
+}
